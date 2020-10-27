9a2e9716e5a2c671c32ef3c87951bd804d6b3702
Administrator <grandorder@vip.qq.com>
2020-10-27 23:43:50 +0800

    修改网页标题

--- src/main/java/com/exchange/ExchangeApplication.java
+++ src/main/java/com/exchange/ExchangeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExchangeApplication {
 
     public static void main(String[] args) {
+        // uuid
         SpringApplication.run(ExchangeApplication.class, args);
     }
 
