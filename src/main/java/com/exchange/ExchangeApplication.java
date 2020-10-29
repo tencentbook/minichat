23b42c02bd9672fd6cd68c0f4f7d697db47deb76
Administrator <grandorder@vip.qq.com>
2020-10-29 22:13:46 +0800

    修改网页标题

--- src/main/java/com/exchange/ExchangeApplication.java
+++ src/main/java/com/exchange/ExchangeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExchangeApplication {
 
     public static void main(String[] args) {
-        // uuid qweet
+        // uuid qweet 的撒法的风格
         SpringApplication.run(ExchangeApplication.class, args);
     }
 
