c302f5d511c529458961f21c5754687751fb5ed8
Administrator <grandorder@vip.qq.com>
2020-10-31 15:28:57 +0800

    修改网页标题

--- src/main/java/com/exchange/ExchangeApplication.java
+++ src/main/java/com/exchange/ExchangeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExchangeApplication {
 
     public static void main(String[] args) {
-        // uuid qweet 的撒法的风格公司的是德国地方 阿萨不好看举报来看你们了你
+        // uuid qweet 的撒法的风格公司的是德国的风土 房管局
         SpringApplication.run(ExchangeApplication.class, args);
     }
 
