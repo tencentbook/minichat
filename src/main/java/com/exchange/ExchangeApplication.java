6aca3ef37e438c194256c6b590696853718c7309
Administrator <grandorder@vip.qq.com>
2020-11-02 00:35:26 +0800

    调整格式

--- src/main/java/com/exchange/ExchangeApplication.java
+++ src/main/java/com/exchange/ExchangeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExchangeApplication {
 
     public static void main(String[] args) {
-        // uuid qweet 的撒法的风格公司的是德国的风土 房管但是公司的是德国是局
+        // uuid qweet 的撒法的风格公司的是德国的风土 房管但是
         SpringApplication.run(ExchangeApplication.class, args);
     }
 
